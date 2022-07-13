8c31b6d3ac89dd7e56a4ac1d06816fdbacd9f944
Francisco García <frangardev@outlook.com>
2022-07-13 18:41:31 -0500

    dynamic open graph

--- src/main.mjs
+++ src/main.mjs
@@ -1,11 +1,24 @@
 import { getParameterByName } from './utils/getId.mjs'
 import { usersPosts } from './utils/getPost.mjs'
 
-
+// Optiene el id de la url
 const prodId = getParameterByName('id');
 console.log('el id es: ', prodId)
 console.log('los posts son: ', usersPosts)
 
-const myPost = usersPosts.filter((item) => item.id == prodId)
+// Determina cual es el post
+const myPost = usersPosts.filter((item) => item.id == prodId)[0]
 
 console.log(myPost)
+
+// Modifica el Open Graph y Twitter Card
+// Imagen
+const ogImage = document.querySelectorAll("#ogImage")
+const twImage = document.querySelectorAll("#twImage")
+ogImage[0].content = myPost.urlImage
+twImage[0].content = myPost.urlImage
+// Descripción
+const ogDescription = document.querySelectorAll("#ogDescription")
+const twDescription = document.querySelectorAll("#twDescription")
+ogDescription[0].content = myPost.description
+twDescription[0].content = myPost.description
