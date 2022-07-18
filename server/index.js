025c8fc6b88c190cfe0c7d6724d12298b478fb45
Francisco García <frangardev@outlook.com>
2022-07-18 18:05:42 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -1,31 +1,31 @@
 const express = require('express')
-const path = require('path');
-const fs = require("fs"); 
-const { getPostById } = require('./stub/posts');
-const app = express();
+const path = require('path')
+const fs = require("fs")
+const { getPostById } = require('./stub/posts')
+const app = express()
 
-const PORT = process.env.PORT || 3000;
-const indexPath  = path.resolve(__dirname, '..', 'index.html');
+const PORT = process.env.PORT || 3000
+const indexPath  = path.resolve(__dirname, '..', 'index.html')
 
 app.use(express.static(
     path.resolve(__dirname, '..', 'build'),
     { maxAge: '30d' },
-));
+))
 
 app.get('/*', (req, res, next) => {
     fs.readFile(indexPath, 'utf8', (err, htmlData) => {
-        if (err) {
-            console.error('Error during file reading', err);
+        if (err) { 
+            console.error('Error al renderizar', err)
             return res.status(404).end()
         }
-        // get post info
+        // trae la información
         const postId = req.query.id;
-        const post = getPostById(postId);
-        if(!post) return res.status(404).send("Post not found");
+        const post = getPostById(postId)
+        if(!post) return res.status(404).send("Post not found")
 
-        // inject meta tags
+        // agregar meta tags
         htmlData = htmlData.replace(
-            "<title>Somos el Cambiooo ;)</title>",
+            "<title>Somos el Cambio</title>",
             `<title>${post.title}</title>`
         )
         .replace('__META_OG_TITLE__', 'Somos el Cambio | ' + post.tag)
@@ -38,7 +38,7 @@ app.get('/*', (req, res, next) => {
 // listening...
 app.listen(PORT, (error) => {
     if (error) {
-        return console.log('Error during app startup', error);
+        return console.log('Error ', error)
     }
-    console.log("listening on " + PORT + "...");
+    console.log("listening on " + PORT + "...")
 });
